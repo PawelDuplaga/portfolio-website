83c0c66a5dbc63f721a1f20efbed0056e4a0caf0
PawelDuplaga <pawelduplagait@gmail.com>
2023-09-11 00:00:15 +0200

    project section done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
       <body className={`${inter.className}
        bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] 
-        rounded-full blur-[10rem] w-[60rem] sm:w-[68.75rem]"></div>
+        rounded-full blur-[10rem] w-[60rem] sm:w-[68.75rem] opacity-80"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] 
-        rounded-full blur-[10rem] w-[60rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        rounded-full blur-[10rem] w-[60rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] opacity-60"></div>
         <Navbar />
         {children}
       </body>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -16,19 +16,33 @@ type ProjectProps = (typeof projectsData)[number]
 
 export const Project = ({title, description, tags, imageUrl}: ProjectProps) => {
     return (
-        <section>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          <ul>
-            {tags.map((tag, index) => (
-              <li key={index}>{tag}</li>
-            ))}
-          </ul>
+        <section className="group bg-gradient-to-bl from-transparent to-slate-200 hover:to-slate-300 max-w-[42rem] border-black/5 overflow-hidden sm:pr-8 relative
+        sm:h-[20rem] mb-3 sm:mb-10 last:mb-0 transition">
+          <div className="py-4 px-5 pb-8 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[50%]">
+            <h3 className="text-2xl font-semibold">{title}</h3>
+            <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+            <ul className="flex flex-wrap gap-2 mt-4 sm:mt-auto">
+              {tags.map((tag, index) => (
+                <li className="bg-gray-50 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-black/60 rounded-full
+                shadow-[-6px_-6px_12px_rgba(255,255,255,0.3),6px_6px_12px_rgba(70,70,70,0.12)]"
+                key={index}>{tag}</li>
+              ))}
+            </ul>
+          </div>
 
           <Image 
             src={imageUrl}
             alt={description}
             quality={95}
+            className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+            group-hover:-translate-x-3
+            group-hover:translate-y-3
+            group-hover:-rotate-2
+            group-hover:scale-[1.1] 
+            group-even:-right-[initial] 
+            group-even:-left-40
+            transition
+            "
           />
         </section>
     )
