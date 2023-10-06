06c1852901594e89e6181701535726282456d498
PawelDuplaga <pawelduplagait@gmail.com>
2023-10-06 19:05:08 +0200

    updated resume

--- src/lib/const/socialsLinks.ts
+++ src/lib/const/socialsLinks.ts
@@ -1,3 +1,3 @@
 export const githubUrl = "https://github.com/PawelDuplaga" as const;
 export const linkedinUrl = "https://www.linkedin.com/in/pawelduplaga/" as const;
-export const resumePdf = "/Pawel_Duplaga_resume.pdf" as const;
+export const resumePdf = "/PawelDuplaga_Resume.pdf" as const;
